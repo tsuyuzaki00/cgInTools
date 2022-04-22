f7e884511f02a5677a97295011b25fa3f90c3e25
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-04-22 19:17:54 +0900

    mgearのskinpack,skinでエクスポートできるスクリプトを作成

--- mgear/library/groupExport.py
+++ mgear/library/groupExport.py
@@ -1,4 +1,7 @@
 import maya.cmds as cmds
+import pymel.core as pm
+from mgear.core import skin
+import os
 
 class GroupExport():
     def grpNodeChoise_query_list(self,grpName,choisList,type="transform"):
@@ -18,11 +21,17 @@ class GroupExport():
     def model_export_list(self,path,objs,file="model"):
         pass
 
-    def objsSkin_export_list(self,path,objs):
-        pass
+    def objsSkin_export_func(self,path,objs):
+        objs=pm.ls(objs)
+        filePath=path
+        for obj in objs:
+            filePath=os.path.join(path,obj+".jSkin")
+            skin.exportSkin(filePath=filePath,objs=[obj])
 
-    def objsSkinPack_export_list(self,path,objs,file="skin"):
-        pass
+    def objsSkinPack_export_func(self,path,objs,file="skin"):
+        objs=pm.ls(objs)
+        packPath=os.path.join(path,file+".gSkinPack")
+        skin.exportJsonSkinPack(packPath=packPath,objs=objs)
 
     def objSence_check_string(self,objName):
         if cmds.objExists(objName):
