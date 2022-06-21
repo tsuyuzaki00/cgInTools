7a095abebdc931bdc966686d81e6444094b13114
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-06-21 19:06:23 +0900

    選択したjointにorientCtrlをコネクトするスクリプトを作成

--- maya/library/offsetCtrl.py
+++ maya/library/offsetCtrl.py
@@ -5,58 +5,110 @@ import maya.cmds as cmds
 class COffsetCtrl():
     def __init__(self,joint):
         self.joint = joint
+        self.parentJnt = cmds.listRelatives(self.joint, parent=True)
+        self.offsetName = self.joint.replace("jnt","offset")
+        self.aimName = self.joint.replace("jnt","aim")
+        self.spaceName = self.joint.replace("jnt","space")
+        self.ctrlName = self.joint.replace("jnt","ctrl")
+        self.trsName = self.joint.replace("jnt","trs")
 
     def run(self):
-        parentJnt = cmds.listRelatives(self.joint, parent=True)
-        offsetName = self.joint.replace("jnt","offset")
-        spaceName = self.joint.replace("jnt","space")
-        ctrlName = self.joint.replace("jnt","ctrl")
-        trsName = self.joint.replace("jnt","trs")
-        grpNode = self.grpNode_check_string(self)
-        
-        parent_list= [
-            {"parent":ctrlName,"child":trsName,},
-            {"parent":spaceName,"child":ctrlName},
-            {"parent":grpNode,"child":spaceName}
-        ]
-        spaceNode = cmds.createNode("transform",n=spaceName)
-        trsNode = cmds.createNode("transform",n=trsName)
-        ctrlNode = cmds.curve(p=[(0, 1.11, 0), (0, 0.78, -0.78), (0, 0, -1.11), (0, -0.78, -0.78), (0, -1.11, 0), (0, -0.78, 0.78), (0, 0, 1.11), (0, 0.78, 0.78), (0, 1.11, 0)] )
-        cmds.rename(ctrlNode ,ctrlName)
+        self.orientCtrl_create_func(self)
 
     def aimCtrl_create_func(self):
+        self.setNodes_create_func()
+        self.aimNode = cmds.createNode("transform",n=self.aimName)
+
+        posSetting_list=[
+            {"copyObj":self.parentJnt,"pasteObj":self.offsetNode},
+            {"copyObj":self.joint,"pasteObj":self.aimNode},
+            {"copyObj":self.joint,"pasteObj":self.spaceNode},
+            {"copyObj":self.joint,"pasteObj":self.ctrlNode},
+            {"copyObj":self.joint,"pasteObj":self.trsNode},
+        ]
+        for posSetting in posSetting_list:
+            self.copyPasteMatrix_edit_func(posSetting["copyObj"],posSetting["pasteObj"])
+
+        parentDicts_list=[
+            {"parent":self.ctrlNode,"child":self.trsNode},
+            {"parent":self.spaceNode,"child":self.ctrlNode},
+            {"parent":self.aimNode,"child":self.spaceNode},
+            {"parent":self.offsetNode,"child":self.aimNode},
+            {"parent":self.grpNode,"child":self.offsetNode},
+        ]
+        for parentDicts in parentDicts_list:
+            cmds.parent(parentDicts["child"],parentDicts["parent"])
         
-        return 
+        attrs=["rotateY","rotateZ"]
+
+        if self.parentJnt == None:
+            self.trsNodeConnect_create_func()
+            for attr in attrs:
+                cmds.setAttr(self.ctrlNode+"."+attr,l=True,k=False,cb=False)
+        elif type(self.parentJnt) is list:
+            self.trsNodeConnect_create_func()
+            for attr in attrs:
+                cmds.setAttr(self.ctrlNode+"."+attr,l=True,k=False,cb=False)
 
     def orientCtrl_create_func(self):
-        if parentSel == []:
-            cmds.parent(spaceNode, 'grp_ctrl_'+ name_ctrl_grp[2])
-            cmds.pointConstraint(trsNode,sel[0])
-            cmds.orientConstraint(trsNode,sel[0])
-        else :
-            offsetName = sel[0].replace("jnt_",'offset_')
-            offsetNode = cmds.createNode("transform", n=offsetName)
-            cmds.parent(offsetNode,parentSel[0])
-            cmds.setAttr(offsetNode+'.translate', 0, 0, 0, type="double3")
-            cmds.setAttr(offsetNode+'.rotate', 0, 0, 0, type="double3")
-            
-            cmds.parent(offsetNode, 'grp_ctrl_'+ name_ctrl_grp[2])
-            cmds.parent(spaceNode,offsetNode)
-            
-            cmds.pointConstraint(parentSel[0],offsetNode)
-            cmds.orientConstraint(parentSel[0],offsetNode)
-            cmds.pointConstraint(trsNode,sel[0])
-            cmds.orientConstraint(trsNode,sel[0])
+        self.setNodes_create_func()
+        posSetting_list=[
+            {"copyObj":self.parentJnt,"pasteObj":self.offsetNode},
+            {"copyObj":self.joint,"pasteObj":self.spaceNode},
+            {"copyObj":self.joint,"pasteObj":self.ctrlNode},
+            {"copyObj":self.joint,"pasteObj":self.trsNode},
+        ]
+        for posSetting in posSetting_list:
+            self.copyPasteMatrix_edit_func(posSetting["copyObj"],posSetting["pasteObj"])
 
-    def parentReset_create_func(self,parent,child):
-        cmds.parent(child,parent)
-        cmds.setAttr(child+'.translate', 0, 0, 0, type="double3")
-        cmds.setAttr(child+'.rotate', 0, 0, 0, type="double3")
+        parentDicts_list=[
+            {"parent":self.ctrlNode,"child":self.trsNode},
+            {"parent":self.spaceNode,"child":self.ctrlNode},
+            {"parent":self.offsetNode,"child":self.spaceNode},
+            {"parent":self.grpNode,"child":self.offsetNode},
+        ]
+        for parentDicts in parentDicts_list:
+            cmds.parent(parentDicts["child"],parentDicts["parent"])
+        
+        if self.parentJnt == None:
+            self.trsNodeConnect_create_func()
+        elif type(self.parentJnt) is list:
+            self.trsNodeConnect_create_func()
+            self.offsetNodeConnect_create_func()
+
+    def offsetNodeConnect_create_func(self,offsetNode="",joint=""):
+        offsetNode = self.offsetNode or offsetNode
+        joint = self.parentJnt or joint
+        #cmds.parentConstraint(self.offsetNode,self.joint)
+        cmds.pointConstraint(joint,offsetNode)
+        cmds.orientConstraint(joint,offsetNode)
+
+    def trsNodeConnect_create_func(self,trsNode="",joint=""):
+        trsNode = self.trsNode or trsNode
+        joint = self.joint or joint
+        #cmds.parentConstraint(self.trsNode,self.joint)
+        cmds.pointConstraint(trsNode,joint)
+        cmds.orientConstraint(trsNode,joint)
+        cmds.scaleConstraint(trsNode,joint)
+
+    def setNodes_create_func(self):
+        self.grpNode = self.grpNode_check_string()
+        self.offsetNode = cmds.createNode("transform",n=self.offsetName)
+        self.spaceNode = cmds.createNode("transform",n=self.spaceName)
+        self.trsNode = cmds.createNode("transform",n=self.trsName)
+        ctrlNode = cmds.curve(p=[(0, 1.11, 0), (0, 0.78, -0.78), (0, 0, -1.11), (0, -0.78, -0.78), (0, -1.11, 0), (0, -0.78, 0.78), (0, 0, 1.11), (0, 0.78, 0.78), (0, 1.11, 0)] )
+        self.ctrlNode = cmds.rename(ctrlNode ,self.ctrlName)
+        return self.grpNode,self.offsetNode,self.spaceNode,self.trsNode,self.ctrlNode
+
+    def copyPasteMatrix_edit_func(self,copyObj,pasteObj):
+        matrix = cmds.xform(copyObj,q=True,ws=True,m=True)
+        cmds.xform(pasteObj,ws=True,m=matrix)
         
     def grpNode_check_string(self):
-        if "grp_ctrl_"+"":
-            grp_node = cmds.createNode("transform", n="grp_ctrl_"+"")
+        grpName_str="ctrls_grp"
+        if cmds.objExists(grpName_str):
+            grp_node = cmds.ls(grpName_str)[0]
             return grp_node
-        else :
-            grp_node = "grp_ctrl_"+""
+        else:
+            grp_node = cmds.createNode("transform", n=grpName_str)
             return grp_node
