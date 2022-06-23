aacd2fe72d34e23110d0bee3180c6c74ae71277f
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-06-23 19:04:53 +0900

    アンロックエラーが起きるので それの対策スクリプトに修正

--- maya/execute/freezeResetHisDel.py
+++ maya/execute/freezeResetHisDel.py
@@ -2,12 +2,14 @@
 import maya.cmds as cmds
 
 def main():
-    sels = cmds.ls(sl = True)
+    sels = cmds.ls(sl=True,dag=True,tr=True)
     for sel in sels:
-        unlocks(sel)
-        cmds.makeIdentity(sel,apply = True, t = 1, r = 1, s = 1, pn = 1)
-        cmds.makeIdentity(sel,apply = False, t = 1, r = 1, s = 1)
-        cmds.delete(sel,ch = True)
+        unlocks(sel)#Prevention of unlock errors
+    for sel in sels:
+        cmds.makeIdentity(sel,apply=True,t=True,r=True,s=True,pn=True)#Freeze Transformations
+        cmds.makeIdentity(sel,apply=False,t=True,r=True,s=True)#Reset Transformations
+        cmds.delete(sel,ch = True)#Delete by Type History
+        cmds.select(cl=True)
 
 def unlocks(sel):
     attrs = ["tx","ty","tz","rx","ry","rz","sx","sy","sz","visibility"]
