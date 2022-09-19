0185fd0e3f1da037ec3b2df64d4cb6e7b7c2e7e9
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-09-19 17:36:13 +0900

    2つジョイントを選んだらoffsetコントローラーを作成するスクリプトを作成

--- maya/execute/_execute.py
+++ maya/execute/_execute.py
@@ -1,11 +1,11 @@
+#import maya.cmds as cmds
+#import pymel.core as pm
 ''' Reload '''
 #from cgInTools.ui import clickSelectUI as UI; reload(UI);
 #from cgInTools.maya.library import click as lib; reload(lib);
 
 ''' Run '''
 #from mgear_guide._tools.Maya.Manager import facialExportCurvesOP as ps
-from cgInTools.maya.manager import clickSelect as ps
+from cgInTools.maya.execute import ctrlOffsetConnect as ps
+from cgInTools.maya.library import cConstraint as cct
 reload(ps); ps.main()
-
-#import pymel.core as pm
-#import maya.cmds as cmd
---
+++ maya/execute/ctrlOffsetConnect.py
@@ -0,0 +1,27 @@
+# -*- coding: iso-8859-15 -*-
+import maya.cmds as cmds
+from cgInTools.maya.library import cConstraint as cct; reload(cct);
+from cgInTools.maya.library import cNaming as cn; reload(cn);
+
+
+def main():
+    offctrl=cct.CtrlParent()
+    joint,offJoint=cmds.ls(sl=True)
+    offctrl.setTargetObj(joint)
+    offctrl.setOffsetJoint(offJoint)
+
+    null=joint.replace("jnt","null")
+    space=joint.replace("jnt","space")
+    trs=joint.replace("jnt","trs")
+    offset=joint.replace("jnt","offset")
+    ctrl=joint.replace("jnt","ctrl")
+
+    offctrl.setSourceObj(trs)
+    offctrl.setOffsetObj(offset)
+
+    offctrl.setSpaceNames([null,space])
+    offctrl.setTrsName(trs)
+    offctrl.setOffsetName(offset)
+    offctrl.setCtrlName(ctrl)
+
+    offctrl.createOffsetConnect()
