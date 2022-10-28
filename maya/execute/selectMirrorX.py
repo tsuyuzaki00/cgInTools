fdd7360aaadf01dcf5cf24aea096f8292c6488a3
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-10-28 18:50:42 +0900

    1番目に選択したものを反X位置に2番目に選択したものを配置するスクリプトを作成

---
+++ maya/execute/selectMirrorX.py
@@ -0,0 +1,12 @@
+# -*- coding: iso-8859-15 -*-
+import maya.cmds as cmds
+import cgInTools as cit
+from cgInTools.maya.library import cMirror as cm
+cit.verReload(cm)
+
+def main():
+    selMirrorX=cm.MatrixMirror()
+    objs=cmds.ls(sl=True)
+    selMirrorX.setSourceNode(objs[0])
+    selMirrorX.setTargetNode(objs[1])
+    selMirrorX.translateOnly()
