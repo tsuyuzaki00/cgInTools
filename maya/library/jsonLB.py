1d682c8a27167d586ce5efe227d6fd29d133c724
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2022-11-04 18:35:02 +0900

    jsonで書き出し読み込みができるようにしたぞーーー！

--- maya/library/jsonLB.py
+++ maya/library/jsonLB.py
@@ -15,7 +15,9 @@ class Json(sb.SetFile):
         """
         self.extension="json" # string
         self.writeDict={}
-        self.packDicts=[]
+        self.writePackDicts=[]
+        self.readDict={}
+        self.readPackDicts=[]
 
     def setWriteDict(self,variable):
         self.writeDict=variable
@@ -34,45 +36,36 @@ class Json(sb.SetFile):
 
     def setPackDicts(self,variable,file=None):
         if self.__getFileJudge(file):
-            self.packDicts=[{file:variable}]
-            return self.packDicts
+            self.writePackDicts=[{"fileName":file,"dataDict":variable}]
+            return self.writePackDicts
         else:
-            self.packDicts=[{self.file:variable}]
-            return self.packDicts
+            self.writePackDicts=[{"fileName":self.file,"dataDict":variable}]
+            return self.writePackDicts
 
     def addPackDicts(self,variable,file=None):
         if self.__getFileJudge(file):
-            self.packDicts.append({file:variable})
-            return self.packDicts
+            self.writePackDicts.append({"fileName":file,"dataDict":variable})
+            return self.writePackDicts
         else:
-            self.packDicts.append({self.file:variable})
-            return self.packDicts
+            self.writePackDicts.append({"fileName":self.file,"dataDict":variable})
+            return self.writePackDicts
 
     def getPackDicts(self):
-        return self.packDicts
+        return self.writePackDicts
 
     def read(self):
-        data_file=self.pathSetting_create_str(self.path,self.file,self.extension)
-        data_dict=self.readJson_quary_dict(data_file)
-        return data_dict
+        self.readDict=self.readJson_quary_dict(self.path,self.file,self.extension)
+        return self.readDict
 
     def readPacks(self):
-        pack_file=self.pathSetting_create_str(self.path,self.file,self.extension)
-        data_files=self.readPack_quary_list(pack_file)
-        data_dicts=[]
-        for data_file in data_files:
-            data_dict=self.readJson_quary_dict(data_file)
-            data_dicts.append(data_dict)
-        return data_dicts
+        self.readPackDicts=self.readPack_quary_list(self.path,self.file,self.extension)
+        return self.readPackDicts
 
     def write(self):
-        data_file=self.pathSetting_create_str(self.path,self.file,self.extension)
-        self.writeJson_create_func(data_file,self.writeDict)
+        self.writeJson_create_func(self.path,self.file,self.extension,self.writeDict)
 
     def writePacks(self):
-        data_file=self.pathSetting_create_str(self.path,self.file,self.extension+"Pack")
-        data_packs=self.packDict_create_list(self.file,self.extension,self.writeDict)
-        self.writePack_create_func(data_packs,data_file)
+        self.writePack_create_func(self.writePackDicts,self.path,self.file,self.extension)
 
     # Jsonファイル名及びパスの設定をする関数
     def pathSetting_create_str(self,path,json_name,extension="json",new_folder=None):
@@ -84,46 +77,44 @@ class Json(sb.SetFile):
             return json_file
 
     # 単体読み込み関数
-    def readJson_quary_dict(self,json_file):
-        with open(json_file, 'r') as f:
+    def readJson_quary_dict(self,path,file,extension):
+        data_file=self.pathSetting_create_str(path,file,extension)
+        with open(data_file, 'r') as f:
             data_dict = json.load(f)
             return data_dict
 
     # パック読み込み関数
-    def readPack_quary_list(self,pack_file):
-        pack_list = self.thisPack_check_str(pack_file)
-        path_list=[]
-        for pack in pack_list:
-            filePath = os.path.join(os.path.split(pack_file)[0], pack)
-            path_list.append(filePath)
-        return path_list
+    def readPack_quary_list(self,path,file,extension):
+        #self.thisPack_check_str(path,file,extension,"packFiles")
+        pack_dict=self.readJson_quary_dict(path,file,extension+"Pack")
+        data_dicts=[]
+        for data_str in pack_dict["packFiles"]:
+            data_dict=self.readJson_quary_dict(path,data_str,extension)
+            data_dicts.append(data_dict)
+        return data_dicts
 
     # 読み込んだdict内に"packFiles"があるか確認する関数
-    def thisPack_check_str(self,pack_file):
-        pack_dict = self.readJson_quary_dict(pack_file)
+    def thisPack_check_str(self,path,file,extension,checkDict):
+        pack_dict=self.readJson_quary_dict(path,file,extension+"Pack")
         try:
-            pack_dict["packFiles"]
-            return pack_dict["packFiles"]
+            pack_dict[checkDict]
+            return pack_dict
         except:
-            cmds.error("There are No packFiles.")
+            cmds.error("setFile is No packFiles.")
 
     # 単体書き出し関数
-    def writeJson_create_func(self,json_file,write_dict):
-        with open(json_file, 'w') as f:
+    def writeJson_create_func(self,path,file,extension,write_dict):
+        data_file=self.pathSetting_create_str(path,file,extension)
+        with open(data_file, 'w') as f:
             json.dump(write_dict,f,indent=4,ensure_ascii=False)
 
-    # packを作成する関数
-    def packDict_create_list(self,subject,extension,write_dict):
-        element_dict={"json_file":subject+"."+extension,"write_dict":write_dict}
-        return element_dict
-        #{"json_file":name.json,"write_dict":{write:...}}
-
     # パック書き出し関数
-    def writePack_create_func(self,packs,json_file):
+    def writePack_create_func(self,pack_dicts,path,file,extension):
+        pack_file=self.pathSetting_create_str(path,file,extension+"Pack")
         packFiles=[]
-        for pack in packs:
-            packFiles.append(pack["json_file"])
-            json_file = os.path.join(self.path,pack["json_file"])
-            self.writeJson_create_func(json_file,pack["write_dict"])
+        for pack_dict in pack_dicts:
+            packFiles.append(pack_dict["fileName"])
+            data_file = os.path.join(path,pack_dict["fileName"]+"."+extension)
+            self.writeJson_create_func(data_file,pack_dict["dataDict"])
         write_dict={"packFiles":packFiles}
-        self.writeJson_create_func(json_file,write_dict)
+        self.writeJson_create_func(pack_file,write_dict)
