30ae89d042e052afa6440854797c7f9d652de9a9
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2023-03-31 19:05:24 +0900

    openMaya番の頂点ID、pointを取得 ターゲットオブジェクトをソースオブジェクトのセンターに移動、 ワールド座標で移動させる関数を作成

--- maya/library/pointLB.py
+++ maya/library/pointLB.py
@@ -10,3 +10,32 @@ def setTargetPos_edit_obj(source,target):
     cmds.move(0,0,0,ls=True)
     cmds.rotate(0,0,0,os=True)
     cmds.parent(source,w=True)
+
+def vertexPos_query_func(vertexs):
+    node_MSelectionList=om2.MSelectionList()
+    for vertex in vertexs:
+        node_MSelectionList.add(vertex)
+    compo_MObject=node_MSelectionList.getComponent(0)[1]
+
+    componentFn=om2.MFnSingleIndexedComponent(compo_MObject)
+    verticesIndex=componentFn.elementCount
+    verticeIDs=componentFn.getElements()
+
+    compo_MDagPath=node_MSelectionList.getComponent(0)[0]
+    mesh_MFnMesh=om2.MFnMesh(compo_MDagPath)
+
+    for vertexID in verticeIDs:
+        point=mesh_MFnMesh.getPoint(vertexID,om2.MSpace.kWorld)
+        print("Vertex ID: ",vertexID, "Position: ",point)
+
+def moveCenter(sources,targets):
+    bb=cmds.xform(targets,q=True,bb=True)
+    centerX=(bb[0]+bb[3])/2
+    centerY=(bb[1]+bb[4])/2
+    centerZ=(bb[2]+bb[5])/2
+    for source in sources:
+        cmds.move(centerX,centerY,centerZ,source,a=True)
+
+def moveWorldPos(source,target):
+    pos=cmds.xform(target,q=True,t=True,ws=True)
+    cmds.move(pos[0],pos[1],pos[2],source,a=True)
