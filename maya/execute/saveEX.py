2f478d026eebe1bb208778d6ca2e72fcee7094e3
tsuyuzaki00 <mugen.tsuyuzaki@gmail.com>
2023-08-08 19:03:48 +0900

    saveEXの更新

--- maya/execute/saveEX.py
+++ maya/execute/saveEX.py
@@ -1,14 +1,19 @@
 import cgInTools as cit
 from ..library import filingLB as fLB
-from ..library import jsonLB as jLB
+from ...library import jsonLB as jLB
 cit.reloads([fLB,jLB])
 
 def main():
-    json_dict=jLB.getJson(cit.mayaData_dir,"save")
+    json_dict=jLB.readJson(cit.mayaData_dir,"save")
     share_dict=json_dict["share_dict"]
-    for save_dict in json_dict.get("save_dicts"):
+    for saveName in json_dict.get("saves"):
+        project=fLB.Project()
+        project.setAbsoluteDirectory(share_dict.get("AbsoluteDirectory"))
+        project.setProjectName(saveName)
+        workDirectory=project.editProject()
+
         save=fLB.File()
-        save.setAbsoluteDirectory(share_dict.get("AbsoluteDirectory"))
-        save.setRelativeDirectory(save_dict.get("RelativeDirectory"))
-        save.setFile(save_dict.get("File"))
+        save.setAbsoluteDirectory(workDirectory)
+        save.setRelativeDirectory("scenes")
+        save.setFile(share_dict.get("firstFile")+saveName+share_dict.get("lastFile"))
         save.save()
